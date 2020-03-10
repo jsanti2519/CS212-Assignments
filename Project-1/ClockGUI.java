ab3813c4ee88722ecd4f6ee63b9e50fbb6d36806
jsanti2519 <53867046+jsanti2519@users.noreply.github.com>
2020-03-10 17:12:38 -0400

    Update ClockGUI.java

--- Project-1/ClockGUI.java
+++ Project-1/ClockGUI.java
@@ -1,6 +1,20 @@
-import java.util.*;
+import java.awt.*;
 import javax.swing.*;
 
 public class ClockGUI extends JFrame {
-
+		public static void setClockGUI() {
+			ClockGUI pane = new ClockGUI();
+		      pane.setTitle("Project 1");
+		      pane.setLayout(new GridLayout(1, 2));
+		      JTextArea clockText = new JTextArea(Project1.unsort); //JLabel won't recognize a new line
+		      pane.getContentPane().add(clockText, BorderLayout.WEST); //layout of sorted array in left side
+		      clockText = new JTextArea(Project1.sort);
+		      clockText.setEditable(false);
+		      pane.getContentPane().add(clockText, BorderLayout.EAST); //layout of sorted array in right side
+		      pane.setSize(1000, 1000);
+		      pane.setLocation(100, 100);
+		      pane.setDefaultCloseOperation(ClockGUI.EXIT_ON_CLOSE);
+		      pane.setVisible(true);
+		}
+	
 }
